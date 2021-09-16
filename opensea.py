eb12744a02bc62b913a10a89338e6eeb7b32167a
Kevin Mok <kmok@redhat.com>
2021-09-16 02:39:23 -0400

    Print filtered, formatted sale summaries

--- opensea.py
+++ opensea.py
@@ -1,5 +1,7 @@
 import constants
 
+from datetime import datetime
+from dateutil import tz
 import json
 from pprint import pprint
 import requests
@@ -29,30 +31,37 @@ def readSalesData(offset):
     with open(getSalesFileName(offset)) as sales_data:
         return json.load(sales_data)
 
+def getEventsList(json):
+    return json["asset_events"]
+
+def getEth(eth):
+    return float(f"{int(eth) * (10 ** -18):.2f}")
+
+def filterSales(sale_events, geThan):
+    return [sale_event for sale_event in sale_events
+            if getEth(sale_event["total_price"]) >= geThan]
+
+def formatTimestamp(timestamp):
+    # 2021-09-16T05:49:06
+    datetime_obj = datetime.strptime(timestamp,
+            '%Y-%m-%dT%H:%M:%S')\
+            .replace(tzinfo=tz.tzutc())\
+            .astimezone(tz.tzlocal())
+    return datetime_obj.strftime('%d %H:%M')
+
+def getSaleSummary(sale):
+    txn_time = formatTimestamp(sale["transaction"]["timestamp"])
+    token_id = sale["asset"]["token_id"]
+    sale_price = getEth(sale["total_price"])
+    return f"{txn_time} | #{token_id:4} = {sale_price} ETH"
+
 if __name__ == "__main__":
-    #  pass
     #  offset = 0
     #  cacheSalesData(getSalesData(offset), offset)
 
-    # print created events
-    #  event_data = readSalesData(1)
-    #  event_types = {}
-    #  for event in event_data["asset_events"]:
-        #  event_types[event["event_type"]] = ''
-    #  created_events = [event for event in event_data["asset_events"]
-            #  if event["event_type"] == "created"]
-    #  #  pprint(created_events)
-    #  with open('created-events.json', 'w') as out:
-        #  json.dump(created_events, out, indent=2)
-
-    #  created price is event["starting_price"] # 
-
-    # get specific token events
-    #  token_id = 500
-    #  querystring = {
-            #  "asset_contract_address": constants.SEVENS_CONTRACT_ADDRESS,
-            #  "token_id": token_id,}
-    #  api_request = requests.request("GET", EVENTS_API,
-            #  headers=API_HEADERS, params=querystring)
-    #  cacheSalesData(json.loads(api_request.text), token_id)
-    pprint(readSalesData(1))
+    sale_events = getEventsList(readSalesData(0))
+    #  with open(getSalesFileName(10), 'w') as out:
+        #  json.dump(sale_events[0], out, indent=2)
+    #  pprint(filterSales(sale_events, 1))
+    for sale in filterSales(sale_events, 3): 
+        print(getSaleSummary(sale))
